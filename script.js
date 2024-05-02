bf7e0f3ee9255e048e04e2dc4af441c22c0e2650
Eliana <127545702+Eliana100@users.noreply.github.com>
2024-05-01 21:19:57 -0300

    Update script.js

--- script.js
+++ script.js
@@ -18,6 +18,26 @@ form.addEventListener('submit', function(event){
 
     let description = '';
     document.getElementById('infos').classList.remove('hidden');
+
+    if (bmi < 18.5) {
+        description = 'Cuidado! Você está abaixo do peso! Podemos te ajudar com o ganho de massa, procure a Smart Fit mais próxima.';
+    } else if (bmi >= 18.5 && bmi <= 25) {
+        description = ' Você esta no peso ideal!Para continuar assim e de forma saudavel, procure a Smart Fit mais próxima.';
+    } else if (bmi > 25 && bmi <= 30) {
+        description = 'Cuidado! Você esta com sobrepeso! Mas, podemos te ajudar com isso. Procure a Smart Fit mais próxima.';
+    } else if (bmi > 30 && bmi <= 35) {
+        description = 'Cuidado! Você esta com obesidade moderada! Procure um de nossos nutricionistas na Smart Fit mais próxima.';
+    } else if (bmi > 35 && bmi <= 40) {
+        description = 'Cuidado! Você esta com obesidade Severa! Procure um de nossos nutricionistas na Smart Fit mais próxima.';
+    } else {
+        description = 'Cuidado! Você esta com obesidade Morbida! Procure um de nossos nutricionistas na Smart Fit mais próxima.';
+    }
+
+    // Formatação do BMI como uma string antes de substituir o ponto por vírgula
+    const bmiFormatted = bmi.toFixed(2).replace('.', ',');
+    value.textContent = bmiFormatted;
+
+    document.getElementById('description').textContent = description;
 });
 
 
